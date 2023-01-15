d7fc377bad6b666e9dd71eab4a1a2135cf849523
MK1998 <86234913+codelearner1998@users.noreply.github.com>
2023-01-15 05:36:11 +0530

    anything file update

--- app.js
+++ app.js
@@ -1,70 +1,58 @@
 // const { response } = require("express");
 const express = require("express");
-const https = require("https")
+const https = require("https");
 
-const bodyParser = require("body-parser")
-
-
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-
-
-app.get("/",function(req,res){
-
-   
-        res.sendFile(__dirname+"/index.html")
-       
-      
-
-})
-
-app.post("/",function(req, res){
-
-
-
-
-  
-
-const query = req.body.cityName
-const apikey = "a247b752bd5f43de11ca6c055780ce85"
-const unit = "metric"
-
-const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+apikey+"&units="+unit
-
-https.get(url, function(response){
-
- 
-
- response.on("data",function(data){            // here data is the all whether data mention in above url
-    const WeatherData = JSON.parse(data)
-    const temp = WeatherData.main.temp
-    const WeatherDescribe = WeatherData.weather[0].description
-    const icon = WeatherData.weather[0].icon
-    const ImageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-
-    console.log(WeatherDescribe);
-   
-    console.log(temp);
-    res.write("<p>The Weather is currently descrbe as  "+ WeatherDescribe + " </p>" )
-    res.write("<h1>The temperature in "+ query + " is "+ temp + " celcius</h1>")
-    res.write("<img src="+ ImageURL + ">");
-    res.send();
-    
-})
- 
-})
-
-
-
-})
-
-
-app.listen(process.env.PORT || 3000 , (req, res) => {
-  console.log("Server started on port 3000");
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const apikey = "a247b752bd5f43de11ca6c055780ce85";
+  const unit = "metric";
 
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apikey +
+    "&units=" +
+    unit;
+
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      // here data is the all whether data mention in above url
+      const WeatherData = JSON.parse(data);
+      const temp = WeatherData.main.temp;
+      const tempMIn = WeatherData.main.temp_min;
+      const tempMax = WeatherData.main.temp_max;
+      const WeatherDescribe = WeatherData.weather[0].description;
+      const icon = WeatherData.weather[0].icon;
+      const ImageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      console.log(WeatherDescribe);
+
+      console.log(temp);
+
+      res.render("data", {
+        myTemp: temp,
+        myMin: tempMIn,
+        myMax: tempMax,
+        myDescribe: WeatherDescribe,
+        myIcon: ImageURL,
+      });
+    });
+  });
+});
+
+app.listen(process.env.PORT || 3000, (req, res) => {
+  console.log("Server started on port 3000");
+});
